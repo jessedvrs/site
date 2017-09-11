47943f10c82d3e9e61367451496cf860ad1f26dc
Jesse de Vries <jessedvrs@gmail.com>
2017-09-11 22:31:29 +0200

    chore(typescript): Re-bootstrap the project with the typescript version of create-react-app

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './styles.css';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                Jesse de Vries
+            </div>
+        );
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './components/App';
+import registerServiceWorker from './services/registerServiceWorker';
+import './stylesheets/index.css';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root') as HTMLElement
+);
+
+registerServiceWorker();
